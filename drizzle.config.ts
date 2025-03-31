acb62904ed1bbbefc45c4a040f2a1ae6edbce49c
yudystriawan <yudistriawan@gmail.com>
2025-03-31 19:21:46 +0800

    Add Drizzle configuration for PostgreSQL database connection

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEON_DATABASE_URL!,
+  },
+});
