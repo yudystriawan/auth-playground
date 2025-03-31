252a7daad2016b7fa8f2adec7e48020f6b789909
yudystriawan <yudistriawan@gmail.com>
2025-03-31 21:31:41 +0800

    Redirect to My Account page after successful password change

--- app/(logged-in)/change-password/_components/change-password-form.tsx
+++ app/(logged-in)/change-password/_components/change-password-form.tsx
@@ -37,7 +37,7 @@ const ChangePasswordForm = () => {
     }
 
     toast.success(response.message ?? "Password changed successfully");
-    router.back();
+    router.push("/my-account");
   };
 
   return (
