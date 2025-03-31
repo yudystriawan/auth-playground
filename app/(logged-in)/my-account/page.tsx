8e8df574046cd47a3303c760e8f053c4ac226ea1
yudystriawan <yudistriawan@gmail.com>
2025-03-31 21:29:29 +0800

    Enhance My Account page to display user's email address from session

--- app/(logged-in)/my-account/page.tsx
+++ app/(logged-in)/my-account/page.tsx
@@ -1,5 +1,21 @@
-const MyAccount = () => {
-  return <div>My Account</div>;
+import { auth } from "@/auth";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
+const MyAccount = async () => {
+  const session = await auth();
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>My Account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email Address</Label>
+        <div className="text-muted-foreground">{session?.user?.email}</div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default MyAccount;
