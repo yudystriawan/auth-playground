4f53a5c8e31d6de91254496e6a8de252b6562a79
yudystriawan <yudistriawan@gmail.com>
2025-03-31 19:36:49 +0800

    Add Toaster component for enhanced user notifications in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <Toaster richColors />
     </html>
   );
 }
