c5824c0a4f4dd15dfe685e950568d5073b46c8f5
yudystriawan <yudistriawan@gmail.com>
2025-04-01 17:18:07 +0800

    Refactor login page layout to use Card component and add navigation links for registration and password reset

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 import LoginForm from "./_components/login-form";
 
 const LoginPage = () => {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LoginForm />
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <div className="text-muted-foreground text-sm">
+          Don&#39;t have an account?{" "}
+          <Link href="/register" className="font-semibold underline">
+            Register
+          </Link>
+        </div>
+        <div className="text-muted-foreground text-sm">
+          Forgot password?{" "}
+          <Link href={`/forgot-password`} className="font-semibold underline">
+            Reset my password
+          </Link>
+        </div>
+      </CardFooter>
+    </Card>
   );
 };
 
