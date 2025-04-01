5176ebb0d5c3b43556814fc12156498e1b28a0bf
yudystriawan <yudistriawan@gmail.com>
2025-04-01 13:01:43 +0800

    Add mailer configuration using nodemailer for email transport

---
+++ lib/email.ts
@@ -0,0 +1,10 @@
+import nodemailer from "nodemailer";
+
+export const mailer = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
